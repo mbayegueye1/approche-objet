33c5a79e5200571a8225af88685320b638a5fa01
bayfall <mbaye1gueye@outlook.com>
2023-11-03 10:30:24 +0100

    Ajout tp4

---
+++ tp4/TestMethodeStatic.java
@@ -0,0 +1,17 @@
+package tp4;
+import java.lang.Integer;
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int valeur = Integer.parseInt(chaine);
+        System.out.println(valeur);
+
+        int a =2;
+        int b =4;
+        int max = Integer.max(a,b);
+        System.out.println(max);
+
+
+
+    }
+}
